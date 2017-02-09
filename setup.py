b83a57a2bae13a76814524bf05df6d021839b21c
Oana Maria Ursu <oursu@scg4-ln01.stanford.edu>
2017-02-09 12:02:46 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+config = {
+    'include_package_data': True,
+    'description': 'Genome Disco: DIfferences in Smoothed COntact maps',
+    'download_url': 'https://github.com/kundajelab/genomedisco',
+    'version': '0.1',
+    'packages': ['genomedisco'],
+    'setup_requires': [],
+    'install_requires': ['numpy>=1.9', 'matplotlib<=1.5.3', 'scikit-learn'],
+    'scripts': ['genomedisco/genomeDisco.py', 'scripts/annotate_baits.py', 'scripts/genomedisco_GenomewideIntraChromosomal.sh','scripts/genomedisco_multiChromosome.sh'],
+    'name': 'genomedisco'
+}
+
+if __name__== '__main__':
+    setup(**config)
