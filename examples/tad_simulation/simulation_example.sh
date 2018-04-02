74129f5901c4c8b97a77fbc012e61d6370951eee
Oana Maria Ursu <oursu@scg4-ln01.stanford.edu>
2018-04-01 23:32:02 -0700

    updated simulation example

--- examples/tad_simulation/simulation_example.sh
+++ examples/tad_simulation/simulation_example.sh
@@ -1,10 +1,10 @@
 mkdir simulation_output
 nodefile=Nodes.w40000.chr21.bed.gz
 
-#python ../../genomedisco/simulations.py  --outdir simulation_output --resolution 40000 --maxdist 5000000 --tadfile tads.chr21.merged.gz --nodefile ${nodefile} --realdatafile HIC003.res40000.chr21.gz --tadmeansize 1000000 --intertadmeandistance 40000 --depth 1000000 --edgenoise 0.0 --nodenoise 0.0 --eps 0.9 --boundarynoise 0 --numsim 1
+python ../../genomedisco/simulations.py  --outdir simulation_output --resolution 40000 --maxdist 5000000 --tadfile tads.chr21.merged.gz --nodefile ${nodefile} --realdatafile HIC003.res40000.chr21.gz --tadmeansize 1000000 --intertadmeandistance 40000 --depth 1000000 --edgenoise 0.0 --nodenoise 0.0 --eps 0.9 --boundarynoise 0 --numsim 1
 
 for f in $(ls simulation_output/*.gz | grep "a.gz\|b.gz");
 do
     ls -lh $f
-    /srv/gsfs0/projects/kundaje/users/oursu/code/anaconda2/mypython/bin/python ../../genomedisco/compute_rw.py --m ${f} --node_file ${nodefile} --mname test --outdir normalized --outpref $(basename ${f}).norm_sqrtvc --tmin 1 --tmax 1 --norm sqrtvc
+    python ../../genomedisco/compute_rw.py --m ${f} --node_file ${nodefile} --mname test --outdir normalized --outpref $(basename ${f}).norm_sqrtvc --tmin 1 --tmax 1 --norm sqrtvc
 done
